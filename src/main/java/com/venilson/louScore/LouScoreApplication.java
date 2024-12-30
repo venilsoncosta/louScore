fd0a15c349cb94d7f11bf228535aadd416785523
Venilson Neves <103954028+venilson0708@users.noreply.github.com>
2024-12-30 16:20:55 -0300

    Cria projeto louScore e cria a entidade Campeonato

---
+++ src/main/java/com/venilson/louScore/LouScoreApplication.java
@@ -0,0 +1,13 @@
+package com.venilson.louScore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouScoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/venilson/louScore/entities/Campeonato.java
@@ -0,0 +1,8 @@
+package com.venilson.louScore.entities;
+
+public class Campeonato {
+
+    private Long id;
+    private String nome;
+
+}
